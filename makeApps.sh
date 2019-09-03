defba9068e8d23a20a64845ffa7862c3440296dd
Fmstrat <root@northwyck>
2019-09-03 16:21:44 -0400

    Added CSS for light theme.

--- makeApps.sh
+++ makeApps.sh
@@ -8,36 +8,45 @@ if [ "$1" == "build" ]; then
 	exit
 fi
 
+
+rm -rf compile binaries
 mkdir -p compile
 mkdir -p binaries
 cp resources/* compile
 chmod a+rw compile -R
 chmod a+rw binaries -R
-rm -rf binaries/*
 cd compile
 
 ## Light apps
 
 # Linux
-docker run -v "${PWD}":/target nativefier --name GoogleVoice -p linux https://voice.google.com /target/
+docker run -v "${PWD}":/target nativefier --name GoogleVoice -p linux --inject /target/light.js https://voice.google.com /target/
 mv google-voice-linux-x64 google-voice
 tar cvfz google-voice-linux.tgz google-voice
 mv google-voice-linux.tgz ../binaries
 
 # Windows
-docker run -v "${PWD}":/target jiahaog/nativefier --name GoogleVoice -i /target/icon.png -p windows https://voice.google.com /target/
+docker run -v "${PWD}":/target jiahaog/nativefier --name GoogleVoice -i /target/icon.png -p windows --inject /target/light.js https://voice.google.com /target/
 mv GoogleVoice-win32-x64 GoogleVoice
 zip -r GoogleVoice-win.zip GoogleVoice
 mv GoogleVoice-win.zip ../binaries
 
 # OSX
-docker run -v "${PWD}":/target jiahaog/nativefier --name GoogleVoice -p osx https://voice.google.com /target/
+docker run -v "${PWD}":/target jiahaog/nativefier --name GoogleVoice -p osx --inject /target/light.js https://voice.google.com /target/
 cd GoogleVoice-darwin-x64
 zip -r GoogleVoice-osx.app.zip GoogleVoice.app
 mv GoogleVoice-osx.app.zip ../../binaries
 cd ..
 
+
 ## Dark apps
+
+cd ..
+rm -rf compile
+mkdir -p compile
+cp resources/* compile
+chmod a+rw compile -R
+cd compile
 
 # Linux
 docker run -v "${PWD}":/target nativefier --name GoogleVoice -p linux --inject /target/dark.js https://voice.google.com /target/
