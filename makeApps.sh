200cdc7cafcede6025ab21d3b4e4daab7c1ae1e7
Fmstrat <nospam@nowsci.com>
2020-03-05 16:39:46 -0500

    Updated build and dark theme

--- makeApps.sh
+++ makeApps.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 if [ "$1" == "build" ]; then
 	cd /tmp
@@ -9,7 +9,7 @@ if [ "$1" == "build" ]; then
 fi
 
 
-rm -rf compile binaries
+sudo rm -rf compile binaries
 mkdir -p compile
 mkdir -p binaries
 cp resources/* compile
@@ -21,18 +21,21 @@ cd compile
 
 # Linux
 docker run -v "${PWD}":/target nativefier --name GoogleVoice -p linux --inject /target/light.js https://voice.google.com /target/
-mv google-voice-linux-x64 google-voice
+sudo chown $(id -u):$(id -g) . -R
+mv GoogleVoice-linux-x64 google-voice
 tar cvfz google-voice-linux.tgz google-voice
 mv google-voice-linux.tgz ../binaries
 
 # Windows
 docker run -v "${PWD}":/target nativefier --name GoogleVoice -i /target/icon.png -p windows --inject /target/light.js https://voice.google.com /target/
+sudo chown $(id -u):$(id -g) . -R
 mv GoogleVoice-win32-x64 GoogleVoice
 zip -r GoogleVoice-win.zip GoogleVoice
 mv GoogleVoice-win.zip ../binaries
 
 # OSX
 docker run -v "${PWD}":/target nativefier --name GoogleVoice -p osx --inject /target/light.js https://voice.google.com /target/
+sudo chown $(id -u):$(id -g) . -R
 cd GoogleVoice-darwin-x64
 zip -r GoogleVoice-osx.app.zip GoogleVoice.app
 mv GoogleVoice-osx.app.zip ../../binaries
@@ -42,7 +45,7 @@ cd ..
 ## Dark apps
 
 cd ..
-rm -rf compile
+sudo rm -rf compile
 mkdir -p compile
 cp resources/* compile
 chmod a+rw compile -R
@@ -50,22 +53,27 @@ cd compile
 
 # Linux
 docker run -v "${PWD}":/target nativefier --name GoogleVoice -p linux --inject /target/dark.js https://voice.google.com /target/
-mv google-voice-linux-x64 google-voice
+sudo chown $(id -u):$(id -g) . -R
+mv GoogleVoice-linux-x64 google-voice
 tar cvfz google-voice-linux-dark.tgz google-voice
 mv google-voice-linux-dark.tgz ../binaries
 
 # Windows
 docker run -v "${PWD}":/target nativefier --name GoogleVoice -i /target/icon.png -p windows --inject /target/dark.js https://voice.google.com /target/
+sudo chown $(id -u):$(id -g) . -R
 mv GoogleVoice-win32-x64 GoogleVoice
 zip -r GoogleVoice-win-dark.zip GoogleVoice
 mv GoogleVoice-win-dark.zip ../binaries
 
 # OSX
 docker run -v "${PWD}":/target nativefier --name GoogleVoice -p osx --inject /target/dark.js https://voice.google.com /target/
+sudo chown $(id -u):$(id -g) . -R
 cd GoogleVoice-darwin-x64
 zip -r GoogleVoice-osx-dark.app.zip GoogleVoice.app
 mv GoogleVoice-osx-dark.app.zip ../../binaries
 cd ..
 
 cd ..
-rm -rf compile
+sudo rm -rf compile
+sudo chown $(id -u):$(id -g) binaries -R
+
