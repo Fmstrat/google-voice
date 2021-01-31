ce97383d28d37adc82f3db8f4a04ea8378d8358f
Than Hutchins <mynockspit@gmail.com>
2021-01-30 19:23:28 -0800

    lie about the user agent to get sign-in to work
    https://github.com/nativefier/nativefier/issues/831

--- makeApps.sh
+++ makeApps.sh
@@ -20,21 +20,21 @@ cd compile
 ## Light apps
 
 # Linux
-docker run -v "${PWD}":/target nativefier --name GoogleVoice -p linux --inject /target/light.js https://voice.google.com /target/
+docker run -v "${PWD}":/target nativefier --name GoogleVoice -p linux --inject /target/light.js --user-agent "Mozilla/5.0 (Windows NT 10.0; rv:74.0) Gecko/20100101 Firefox/74.0" https://voice.google.com /target/
 sudo chown $(id -u):$(id -g) . -R
 mv GoogleVoice-linux-x64 google-voice
 tar cvfz google-voice-linux.tgz google-voice
 mv google-voice-linux.tgz ../binaries
 
 # Windows
-docker run -v "${PWD}":/target nativefier --name GoogleVoice -i /target/icon.png -p windows --inject /target/light.js https://voice.google.com /target/
+docker run -v "${PWD}":/target nativefier --name GoogleVoice -i /target/icon.png -p windows --inject /target/light.js --user-agent "Mozilla/5.0 (Windows NT 10.0; rv:74.0) Gecko/20100101 Firefox/74.0" https://voice.google.com /target/
 sudo chown $(id -u):$(id -g) . -R
 mv GoogleVoice-win32-x64 GoogleVoice
 zip -r GoogleVoice-win.zip GoogleVoice
 mv GoogleVoice-win.zip ../binaries
 
 # OSX
-docker run -v "${PWD}":/target nativefier --name GoogleVoice -p osx --inject /target/light.js https://voice.google.com /target/
+docker run -v "${PWD}":/target nativefier --name GoogleVoice -p osx --inject /target/light.js --user-agent "Mozilla/5.0 (Windows NT 10.0; rv:74.0) Gecko/20100101 Firefox/74.0" https://voice.google.com /target/
 sudo chown $(id -u):$(id -g) . -R
 cd GoogleVoice-darwin-x64
 zip -r GoogleVoice-osx.app.zip GoogleVoice.app
@@ -52,21 +52,21 @@ chmod a+rw compile -R
 cd compile
 
 # Linux
-docker run -v "${PWD}":/target nativefier --name GoogleVoice -p linux --inject /target/dark.js https://voice.google.com /target/
+docker run -v "${PWD}":/target nativefier --name GoogleVoice -p linux --inject /target/dark.js --user-agent "Mozilla/5.0 (Windows NT 10.0; rv:74.0) Gecko/20100101 Firefox/74.0" https://voice.google.com /target/
 sudo chown $(id -u):$(id -g) . -R
 mv GoogleVoice-linux-x64 google-voice
 tar cvfz google-voice-linux-dark.tgz google-voice
 mv google-voice-linux-dark.tgz ../binaries
 
 # Windows
-docker run -v "${PWD}":/target nativefier --name GoogleVoice -i /target/icon.png -p windows --inject /target/dark.js https://voice.google.com /target/
+docker run -v "${PWD}":/target nativefier --name GoogleVoice -i /target/icon.png -p windows --inject /target/dark.js --user-agent "Mozilla/5.0 (Windows NT 10.0; rv:74.0) Gecko/20100101 Firefox/74.0" https://voice.google.com /target/
 sudo chown $(id -u):$(id -g) . -R
 mv GoogleVoice-win32-x64 GoogleVoice
 zip -r GoogleVoice-win-dark.zip GoogleVoice
 mv GoogleVoice-win-dark.zip ../binaries
 
 # OSX
-docker run -v "${PWD}":/target nativefier --name GoogleVoice -p osx --inject /target/dark.js https://voice.google.com /target/
+docker run -v "${PWD}":/target nativefier --name GoogleVoice -p osx --inject /target/dark.js --user-agent "Mozilla/5.0 (Windows NT 10.0; rv:74.0) Gecko/20100101 Firefox/74.0" https://voice.google.com /target/
 sudo chown $(id -u):$(id -g) . -R
 cd GoogleVoice-darwin-x64
 zip -r GoogleVoice-osx-dark.app.zip GoogleVoice.app
